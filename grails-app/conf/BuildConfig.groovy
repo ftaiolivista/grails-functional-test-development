c0e234a5b0983d41e6ae199af7eadf2272afa544
Fabiano Taioli <ftaioli@vista.it>
2013-07-23 18:04:14 +0200

    varius dependencies update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,33 +6,37 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
-    inherits("global")
-    log "warn"
-    checksums true
-    repositories {
-        inherits true
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-        mavenLocal()
-        mavenCentral()
-    }
-    dependencies {
-        test "org.codehaus.geb:geb-junit4:0.7.2"
-        test "org.seleniumhq.selenium:selenium-chrome-driver:2.25.0"
-    }
-    plugins {
-        runtime ":hibernate:$grailsVersion", {
-			export = false
-		}
-        test ":geb:0.7.2", ":spock:0.6", ":remote-control:1.3", {
-            export = false
-        }
-        build ":release:2.0.4", ":tomcat:$grailsVersion", {
-            export = false
-            exclude "groovy"
-        }
-    }
+	inherits("global")
+	log "warn"
+	checksums true
+	def gebVersion = '0.9.0'
+	def seleniumVersion = '2.21.0'
+	def spockVersion = '0.7'
+	repositories {
+		inherits true
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+	}
+	dependencies {
+		test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") { exclude "xml-apis" }
+		test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
+		test("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
+
+		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+		test "org.gebish:geb-spock:$gebVersion"
+	}
+	plugins {
+		
+		test 	":remote-control:1.4", {export = false}
+		test 	":spock:$spockVersion",{ export= false}
+		test	":geb:$gebVersion",{ export= false}
+		
+		build(":tomcat:$grailsVersion",":release:2.2.0",":rest-client-builder:1.0.3") { export = false }
+		compile ":hibernate:$grailsVersion"
+	}
 }
 
 grails.release.scm.enabled = false
